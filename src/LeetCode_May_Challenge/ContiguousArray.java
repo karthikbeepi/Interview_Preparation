4eaed90e371553c0b139fa44bb38cbf42a816e0f
karthikbeepi <karthikbeepi@gmail.com>
2020-06-01 20:04:06 -0500

    Create ContiguousArray.java

---
+++ src/LeetCode_May_Challenge/ContiguousArray.java
@@ -0,0 +1,13 @@
+package LeetCode_May_Challenge;
+
+public class ContiguousArray {
+    
+    public int findMaxLength(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+        ContiguousArray ob = new ContiguousArray();
+        System.out.println(ob.findMaxLength(new int[]{0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1}));
+    }
+}
